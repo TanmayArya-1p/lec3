25e38adc2da3c0d1e74f7b6919e2f9878d0150d1
TanmayArya-1p <tanmayarya2018@gmail.com>
2025-04-18 03:29:18 +0530

    revamped attack formula

--- duel/battle.js
+++ duel/battle.js
@@ -39,18 +39,21 @@ class BattlePokemon {
 
     attack(target, move,damage=null) {  
               
-        const movePower = move.power;
         const moveAccuracy = move.accuracy;
-
         if (damage!==null && Math.random() * 100 > moveAccuracy) {
             return 0;
         }
 
         if(damage===null){
-            const randomFactor = Math.random() * (1 - 0.9) + 0.2;
+            console.log(move)
+            const stab = this.pokemon.types.map(a=>{
+                return a.name
+            }).includes(move.type.name) ? 1.5 : 1;
+            const eff = this.getTypeEffectiveness(move, target.pokemon);
+            const rf = Math.random() * (1 - 0.85) + 0.85;
             damage = Math.floor(
-                (movePower * (this.pokemon.stats.attack / target.pokemon.stats.defense)) * randomFactor
-            );  
+                (move.power * (this.pokemon.stats.attack / target.pokemon.stats.defense)) * stab * eff * rf
+            );
         }
         target.hp -= damage;
         target.hp = Math.max(target.hp, 0);
@@ -63,6 +66,178 @@ class BattlePokemon {
     heal(amount) {
         this.hp = Math.min(this.hp + amount, this.pokemon.stats.hp);
     }
+    calculateTypeEffectiveness(moveType, defenderTypes) {
+        const typeEffectiveness = {
+        normal: {
+            rock: 0.5,
+            ghost: 0,
+            steel: 0.5
+        },
+        fire: {
+            fire: 0.5,
+            water: 0.5,
+            grass: 2,
+            ice: 2,
+            bug: 2,
+            rock: 0.5,
+            dragon: 0.5,
+            steel: 2
+        },
+        water: {
+            fire: 2,
+            water: 0.5,
+            grass: 0.5,
+            ground: 2,
+            rock: 2,
+            dragon: 0.5
+        },
+        electric: {
+            water: 2,
+            electric: 0.5,
+            grass: 0.5,
+            ground: 0,
+            flying: 2,
+            dragon: 0.5
+        },
+        grass: {
+            fire: 0.5,
+            water: 2,
+            grass: 0.5,
+            poison: 0.5,
+            ground: 2,
+            flying: 0.5,
+            bug: 0.5,
+            rock: 2,
+            dragon: 0.5,
+            steel: 0.5
+        },
+        ice: {
+            fire: 0.5,
+            water: 0.5,
+            grass: 2,
+            ice: 0.5,
+            ground: 2,
+            flying: 2,
+            dragon: 2,
+            steel: 0.5
+        },
+        fighting: {
+            normal: 2,
+            ice: 2,
+            poison: 0.5,
+            flying: 0.5,
+            psychic: 0.5,
+            bug: 0.5,
+            rock: 2,
+            ghost: 0,
+            dark: 2,
+            steel: 2,
+            fairy: 0.5
+        },
+        poison: {
+            grass: 2,
+            poison: 0.5,
+            ground: 0.5,
+            rock: 0.5,
+            ghost: 0.5,
+            steel: 0,
+            fairy: 2
+        },
+        ground: {
+            fire: 2,
+            electric: 2,
+            grass: 0.5,
+            poison: 2,
+            flying: 0,
+            bug: 0.5,
+            rock: 2,
+            steel: 2
+        },
+        flying: {
+            electric: 0.5,
+            grass: 2,
+            fighting: 2,
+            bug: 2,
+            rock: 0.5,
+            steel: 0.5
+        },
+        psychic: {
+            fighting: 2,
+            poison: 2,
+            psychic: 0.5,
+            dark: 0,
+            steel: 0.5
+        },
+        bug: {
+            fire: 0.5,
+            grass: 2,
+            fighting: 0.5,
+            poison: 0.5,
+            flying: 0.5,
+            psychic: 2,
+            ghost: 0.5,
+            dark: 2,
+            steel: 0.5,
+            fairy: 0.5
+        },
+        rock: {
+            fire: 2,
+            ice: 2,
+            fighting: 0.5,
+            ground: 0.5,
+            flying: 2,
+            bug: 2,
+            steel: 0.5
+        },
+        ghost: {
+            normal: 0,
+            psychic: 2,
+            ghost: 2,
+            dark: 0.5
+        },
+        dragon: {
+            dragon: 2,
+            steel: 0.5,
+            fairy: 0
+        },
+        dark: {
+            fighting: 0.5,
+            psychic: 2,
+            ghost: 2,
+            dark: 0.5,
+            fairy: 0.5
+        },
+        steel: {
+            fire: 0.5,
+            water: 0.5,
+            electric: 0.5,
+            ice: 2,
+            rock: 2,
+            steel: 0.5,
+            fairy: 2
+        },
+        fairy: {
+            fire: 0.5,
+            fighting: 2,
+            poison: 0.5,
+            dragon: 2,
+            dark: 2,
+            steel: 0.5
+        }
+        };
+        let eff = 1;
+        for (let i of defenderTypes) {
+            eff *= (typeEffectiveness[moveType]?.[i] || 1);
+        }
+        return eff;
+    }
+    getTypeEffectiveness(move, defender) {
+        const moveType = move.type.name;
+        console.log("DEFENDER",defender)
+        const defenderTypes = defender.types.map(t => t.name);
+        return this.calculateTypeEffectiveness(moveType, defenderTypes)/5;
+    }
+
 
 }
 
@@ -129,6 +304,7 @@ class BattleSimulator {
         for(let i=0; i<this.homePokemon.pokemon.moves.length; i++){
             let move = this.homePokemon.pokemon.moves[i];
             let attackButton = document.getElementById(`attack-${i+1}`) 
+            move.pp = Math.floor(move.pp/2);
             move.currPP = move.pp;
             
             document.getElementById(`attack-${i+1}-text`).innerText = move.name;
--- pokemon.js
+++ pokemon.js
@@ -2,13 +2,14 @@ class Pokemon{
     constructor(code) {
         this.code = code;
         this.hydrated=false
-    }
+    }   
 
     async hydrateData() {
         let res = await fetch("https://pokeapi.co/api/v2/pokemon/"+this.code)
         let json = await res.json()
         this.name = json.name;
         this.moves = json.moves.map(move => move.move);
+        this.height = json.height;
         this.types = json.types.map(type => type.type);
         this.sprites = json.sprites;
         this.abilities = json.abilities.map(ability => ability.ability);
